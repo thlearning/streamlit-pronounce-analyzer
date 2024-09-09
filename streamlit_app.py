08f4520bd9850a8e8d416726cd27a8a42f0f469c
thlearning <125428564+thlearning@users.noreply.github.com>
2024-09-09 12:23:27 +0000

    add audiorecorder

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,29 @@
 import streamlit as st
 from st_audiorec import st_audiorec
+from audiorecorder import audiorecorder
 
 st.title("Pronounce-Analyzer")
 st.write(
     "using Streamlit Audio Recorder."
 )
 
+#########################################
+
 wav_audio_data = st_audiorec()
 
 if wav_audio_data is not None:
     st.audio(wav_audio_data, format='audio/wav')
+
+#########################################
+
+audio = audiorecorder("Click to record", "Click to stop recording")
+
+if len(audio) > 0:
+    # To play audio in frontend:
+    st.audio(audio.export().read())  
+
+    # To save audio to a file, use pydub export method:
+    audio.export("audio.wav", format="wav")
+
+    # To get audio properties, use pydub AudioSegment properties:
+    st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
