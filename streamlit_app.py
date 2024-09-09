1da899d6439cb968827161ddf7ca37fff917c5e0
thlearning <125428564+thlearning@users.noreply.github.com>
2024-09-09 12:19:17 +0000

    title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from st_audiorec import st_audiorec
 
+st.title("Pronounce-Analyzer")
+st.write(
+    "using Streamlit Audio Recorder."
+)
+
 wav_audio_data = st_audiorec()
 
 if wav_audio_data is not None:
     st.audio(wav_audio_data, format='audio/wav')
-
-#st.title("🎈 My new app")
-#st.write(
-#    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-#)
