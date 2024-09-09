d9ba0f8c275a99177c4bebcfd3151452936dd2eb
thlearning <125428564+thlearning@users.noreply.github.com>
2024-09-09 12:03:14 +0000

    blank

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+# from audiorecorder import audiorecorder
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# st.title("Audio Recorder")
+# audio = audiorecorder("Click to record", "Click to stop recording")
+
+#if len(audio) > 0:
+#    # To play audio in frontend:
+#    st.audio(audio.export().read())  
+
+#    # To save audio to a file, use pydub export method:
+#    audio.export("audio.wav", format="wav")
+
+#    # To get audio properties, use pydub AudioSegment properties:
+#    st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
+
+#st.title("🎈 My new app")
+#st.write(
+#    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+#)
