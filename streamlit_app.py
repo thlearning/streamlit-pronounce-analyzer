8727a5d38b80b50d3932f262452ba7b9bf233979
thlearning <125428564+thlearning@users.noreply.github.com>
2024-09-13 15:16:18 +0000

    audio

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,17 +16,6 @@ wav_audio_data = st_audiorec()
 
 if wav_audio_data is not None:
     st.audio(wav_audio_data, format='audio/wav')
-    y, sr = librosa.load('believe.mp3', sr=16000)
-    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
-
-    fig, ax = plt.subplots()
-    S_dB = librosa.power_to_db(S, ref=np.max)
-    img = librosa.display.specshow(S_dB, x_axis='time',
-                             y_axis='mel', sr=sr,
-                             fmax=8000, ax=ax)
-    fig.colorbar(img, ax=ax, format='%+2.0f dB')
-    ax.set(title='Mel-frequency spectrogram')
-    st.pyplot(fig)
 
 #########################################
 
@@ -45,6 +34,19 @@ if len(audio) > 0:
 
     # To get audio properties, use pydub AudioSegment properties:
     st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
+
+    # Draw spectrogram!
+    y, sr = librosa.load(audio, sr=16000)
+    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
+
+    fig, ax = plt.subplots()
+    S_dB = librosa.power_to_db(S, ref=np.max)
+    img = librosa.display.specshow(S_dB, x_axis='time',
+                             y_axis='mel', sr=sr,
+                             fmax=8000, ax=ax)
+    fig.colorbar(img, ax=ax, format='%+2.0f dB')
+    ax.set(title='Mel-frequency spectrogram')
+    st.pyplot(fig)
 
 #########################################
 st.write(
