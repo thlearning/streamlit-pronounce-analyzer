8c5cf4d287f34c770a97fed3a27537f4b408f000
thlearning <125428564+thlearning@users.noreply.github.com>
2024-09-13 15:34:01 +0000

    rec

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from st_audiorec import st_audiorec
+#from st_audiorec import st_audiorec
 from audiorecorder import audiorecorder
 import librosa
 import matplotlib.pyplot as plt
@@ -11,12 +11,10 @@ st.write(
 )
 
 #########################################
-
-wav_audio_data = st_audiorec()
-
-if wav_audio_data is not None:
-    st.audio(wav_audio_data, format='audio/wav')
-
+#wav_audio_data = st_audiorec()
+#
+#if wav_audio_data is not None:
+#    st.audio(wav_audio_data, format='audio/wav')
 #########################################
 
 st.write(
