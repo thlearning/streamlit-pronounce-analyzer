87226d696722f9097927051b1a7c386b588b4201
thlearning <125428564+thlearning@users.noreply.github.com>
2024-09-13 14:43:32 +0000

    matplotlib

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from st_audiorec import st_audiorec
 from audiorecorder import audiorecorder
 import librosa
+import matplotlib.pyplot as plt
 
 st.title("Pronounce-Analyzer")
 st.write(
