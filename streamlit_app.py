1721a98f3937ba0360245ccffd85c4a54dfa28ec
thlearning <125428564+thlearning@users.noreply.github.com>
2024-09-14 11:34:02 +0000

    1024

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ fig, ax = plt.subplots()
 #img = librosa.display.specshow(S_dB, x_axis='time', y_axis='mel', sr=sr, fmax=6000, ax=ax)
 
 hop_length = 512
-D = librosa.amplitude_to_db(np.abs(librosa.stft(y, n_fft=512, hop_length=hop_length)), ref=np.max)
+D = librosa.amplitude_to_db(np.abs(librosa.stft(y, n_fft=1024, hop_length=hop_length)), ref=np.max)
 img = librosa.display.specshow(D, x_axis='time', y_axis='log', hop_length=hop_length, sr=sr, ax=ax)
 
 fig.colorbar(img, ax=ax, format='%+2.0f dB')
