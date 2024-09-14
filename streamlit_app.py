78dbdd98ff765f53761b25b5d0a75187d3af8760
thlearning <125428564+thlearning@users.noreply.github.com>
2024-09-14 01:21:41 +0000

    two spec

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,11 +61,11 @@ S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=6000)
 
 fig, ax = plt.subplots()
 S_dB = librosa.power_to_db(S, ref=np.max)
-#img = librosa.display.specshow(S_dB, x_axis='time', y_axis='mel', sr=sr, fmax=8000, ax=ax)
+img = librosa.display.specshow(S_dB, x_axis='time', y_axis='mel', sr=sr, fmax=8000, ax=ax)
 
 hop_length = 1024
 D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
-librosa.display.specshow(D, y_axis='mel', sr=sr, x_axis='time', ax=ax)
+librosa.display.specshow(D, y_axis='log', sr=sr, x_axis='time', ax=ax)
 
 fig.colorbar(img, ax=ax, format='%+2.0f dB')
 ax.set(title='Mel-frequency spectrogram')
