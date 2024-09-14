49a2278e960150028e8c6336d4c09e20917874df
thlearning <125428564+thlearning@users.noreply.github.com>
2024-09-14 14:45:26 +0000

    fig

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,6 +58,8 @@ st.audio(audio_bytes, format='audio/mpeg')
 # Draw spectrogram! ################################
 y, sr = librosa.load('believe.mp3', sr=16000)
 
+fig, ax = plt.subplots()
+
 #############
 S = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=1024, hop_length=32, n_mels=256, fmax=8000)
 S_dB = librosa.power_to_db(S, ref=np.max)
@@ -71,7 +73,6 @@ D_db = librosa.amplitude_to_db(D, ref=np.max)
 img = librosa.display.specshow(D_db, x_axis='time', y_axis='log', hop_length=hop_length, sr=sr, ax=ax)
 #############
 
-fig, ax = plt.subplots()
 fig.colorbar(img, ax=ax, format='%+2.0f dB')
 ax.set(title='Mel-frequency spectrogram')
 st.pyplot(fig)
