7116b8c4614be5ca1198945c3a3e8cdb8686de91
thlearning <125428564+thlearning@users.noreply.github.com>
2024-09-20 14:33:50 +0000

    all

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,8 +48,8 @@ if len(audio) > 0:
     D_db = librosa.amplitude_to_db(D, ref=np.max)
     
     img = librosa.display.specshow(D_db, x_axis='time', y_axis='log', hop_length=hop_length, sr=sr, ax=ax[1])
-    ax.set(title='Spectrogram')
-    #fig.colorbar(img, ax=ax, format='%+2.0f dB')
+    #ax[1].set(title='Spectrogram')
+    #fig.colorbar(img, ax=ax[1], format='%+2.0f dB')
     
     st.pyplot(fig)
 
